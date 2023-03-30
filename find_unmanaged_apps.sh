b89110be8e87b0cba9686da6a59f52dafad1a56c
David Veksler <heroic@gmail.com>
2023-03-30 15:33:06 -0600

    tmp

--- find_unmanaged_apps.sh
+++ find_unmanaged_apps.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-# Get the list of installed applications in the /Applications folder
-installed_apps=$(ls /Applications | sed 's/.app$//')
+# Get a list of installed apps in /Applications
+installed_apps=$(find /Applications -maxdepth 1 -iname "*.app")
 
-# Get the list of Homebrew-installed applications
-homebrew_apps=$(brew list --cask)
-
-# Loop through installed applications and check if they're in Homebrew Casks and not managed by Homebrew
+# Check if each app is available in Homebrew
 for app in $installed_apps; do
-  # Search for the app in Homebrew Casks
-  search_result=$(brew search --casks "^$app$" 2> /dev/null)
+  bundle_id=$(mdls -name kMDItemCFBundleIdentifier -raw "$app")
+  app_name=$(basename "$app" .app)
 
-  # Check if the app is in the search result and not managed by Homebrew
-  if [[ ! -z "$search_result" ]] && ! [[ $homebrew_apps =~ (^|[[:space:]])"$app"($|[[:space:]]) ]]; then
-    echo "$app is available in Homebrew but not managed by Homebrew"
+  # Check if the app's name is available in Homebrew
+  echo "Checking for $app_name - $bundle_id: "
+
+  # Search for an exact match in Homebrew
+  if brew info "$app_name" >/dev/null 2>&1; then
+    cask_or_formula_name="$app_name"
+
+    # Check if the app is managed by Homebrew
+    app_managed_by_homebrew=$(brew list --cask | grep -E "^$cask_or_formula_name$" || brew list | grep -E "^$cask_or_formula_name$")
+
+    if [[ -z "$app_managed_by_homebrew" ]]; then
+      echo "$app_name is available in Homebrew as $cask_or_formula_name but not managed by it."
+
+      # Prompt the user to replace the app with the Homebrew version
+      read -p "Do you want to replace $app_name with the Homebrew version? (y/n) " choice
+
+      if [[ $choice == "y" ]]; then
+        # Remove the existing app
+        rm -rf "$app"
+
+        # Install the Homebrew version
+        if brew install --cask "$cask_or_formula_name" >/dev/null 2>&1; then
+          echo "$app_name replaced with the Homebrew cask version."
+        elif brew install "$cask_or_formula_name"; then
+          echo "$app_name replaced with the Homebrew formula version."
+        else
+          echo "Failed to replace $app_name with the Homebrew version."
+        fi
+      fi
+    fi
   fi
 done
