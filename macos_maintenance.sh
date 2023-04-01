647e059640a2f406bfc954693c860cf4331cbaf4
David Veksler <heroic@gmail.com>
2023-04-01 13:49:25 -0600

    improve logging

--- macos_maintenance.sh
+++ macos_maintenance.sh
@@ -1,89 +1,61 @@
 #!/bin/bash
 
-LOG_FILE="maintenance_$(date '+%Y%m%d_%H%M%S').log"
-
-# Function to log messages and errors
+# Log function
 log() {
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "${LOG_FILE}"
-}
-
-# Progress indicator with timestamp
-spinner() {
-  local pid=$!
-  local delay=0.1
-  local spinstr='\|/-'
-  local start_time=$(date +%s)
-  while [ "$(ps a | awk '{print $1}' | grep ${pid})" ]; do
-    local temp=${spinstr#?}
-    local elapsed_time=$(( $(date +%s) - start_time ))
-    printf " [%c] Time: %ds " "${spinstr}" "${elapsed_time}"
-    local spinstr=${temp}${spinstr%"${temp}"}
-    sleep ${delay}
-    printf "\b\b\b\b\b\b\b\b\b\b\b"
-  done
-  printf "          \b\b\b\b\b\b\b\b\b\b"
+  echo "[$(date "+%Y-%m-%d %H:%M:%S")] $1"
 }
 
 # Prompt for sudo password
 echo "Please enter your sudo password:"
-read -s SUDO_PASSWORD
-echo
+read -s PASSWORD
 
-# Check if Homebrew is installed
-if ! command -v brew &> /dev/null; then
-  log "Homebrew is not installed. Install it from https://brew.sh and try again."
-  exit 1
-fi
-
-# Check if mas is installed
-if ! command -v mas &> /dev/null; then
-  log "mas is not installed. Install it using 'brew install mas' and try again."
-  exit 1
-fi
-
-# Update macOS and software
+# Update macOS and installed software
 log "Updating macOS and installed software"
-softwareupdate -ia --verbose & spinner
+echo $PASSWORD | sudo -S softwareupdate -ia & sleep 2
 
+# Update Homebrew and installed packages
 log "Updating Homebrew and installed packages"
-brew update && brew upgrade && brew cleanup & spinner
+brew update & sleep 2
+brew upgrade & sleep 2
 
+# Update Mac App Store applications
 log "Updating Mac App Store applications"
-mas upgrade & spinner
+mas upgrade & sleep 2
 
-# Clear system caches
+# Clearing system caches
 log "Clearing system caches"
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf /Library/Caches/* & spinner
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf ~/Library/Caches/* & spinner
+echo $PASSWORD | sudo -S rm -rf /Library/Caches/* & sleep 2
+echo $PASSWORD | sudo -S rm -rf /System/Library/Caches/* & sleep 2
 
-# Clear logs
+# Clearing system logs
 log "Clearing system logs"
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf /private/var/log/* & spinner
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf ~/Library/Logs/* & spinner
+echo $PASSWORD | sudo -S rm -rf /private/var/log/* & sleep 2
 
-# Clear temporary files
+# Clearing temporary files
 log "Clearing temporary files"
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf /private/var/tmp/* & spinner
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf /private/var/folders/* & spinner
-echo "${SUDO_PASSWORD}" | sudo -S rm -rf /tmp/* & spinner
+echo $PASSWORD | sudo -S rm -rf /private/var/tmp/* & sleep 2
+echo $PASSWORD | sudo -S rm -rf /private/var/folders/*/*/*/C/* & sleep 2
+echo $PASSWORD | sudo -S rm -rf /private/var/folders/*/*/*/T/* & sleep 2
+echo $PASSWORD | sudo -S rm -rf /private/var/folders/*/*/*/X/* & sleep 2
 
-# Clear browser caches (Safari, Chrome, Firefox, Edge, and Brave)
+# Clearing browser caches
 log "Clearing browser caches"
-rm -rf ~/Library/Safari/LocalStorage & spinner
-rm -rf ~/Library/Caches/com.apple.Safari & spinner
-rm -rf ~/Library/Caches/com.apple.WebKit.PluginProcess & spinner
-rm -rf ~/Library/Caches/Google/Chrome & spinner
-rm -rf ~/Library/Caches/Mozilla/Firefox & spinner
-rm -rf ~/Library/Caches/MicrosoftEdge & spinner
-rm -rf ~/Library/Caches/BraveSoftware/Brave-Browser & spinner
+rm -rf ~/Library/Caches/com.apple.WebKit.PluginProcess & sleep 2
+rm -rf ~/Library/Caches/com.apple.WebKit.WebContent & sleep 2
+rm -rf ~/Library/Caches/com.google.Chrome & sleep 2
+rm -rf ~/Library/Caches/Google/Chrome & sleep 2
+rm -rf ~/Library/Caches/Mozilla/Firefox & sleep 2
+rm -rf ~/Library/Caches/com.microsoft.edgemac & sleep 2
+rm -rf ~/Library/Caches/com.brave.Browser & sleep 2
 
 # Optimize system performance
 log "Optimizing system performance"
-echo "${SUDO_PASSWORD}" | sudo periodic daily weekly monthly & spinner
+echo $PASSWORD | sudo -S periodic daily weekly monthly & sleep 2
 
 # Verify and repair disk permissions
 log "Verifying and repairing disk permissions"
-echo "${SUDO_PASSWORD}" | sudo diskutil verifyVolume / & spinner
-echo "${SUDO_PASSWORD}" | sudo diskutil repairVolume / & spinner
+echo $PASSWORD | sudo -S diskutil verifyVolume / & sleep 2
+echo $PASSWORD | sudo -S diskutil repairVolume / & sleep 2
 
-echo "System maintenance complete."
+# System maintenance complete
+log "System maintenance complete."
