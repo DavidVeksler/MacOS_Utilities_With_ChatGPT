82d23a1cc949b974cefc3192732749cd4394d0b1
David Veksler <heroic@gmail.com>
2024-01-16 16:07:10 -0700

    add description

--- startup_inspector.sh
+++ startup_inspector.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Description: This script lists non-Apple startup items, including login items, launch agents, and daemons on a macOS system.
+
 # ANSI Color Codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
